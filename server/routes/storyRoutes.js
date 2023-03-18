da6ab802b624cd88b37b2e8f9f7c4f943fac7026
Tygo <tygo.geervliet@hva.nl>
2023-03-18 10:33:43 +0100

    added comment

--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -17,54 +17,60 @@ class storyRoutes {
 
 
     #addStory() {
+        // Handle POST request to add a story
+        this.#app.post("/story/add", this.#multer().single("file"), async (req, res) => {
+            try {
+                // Extract relevant data from the request
+                const { body: { subject, story, year }, file: { buffer } } = req;
+                console.log(req.file);
 
-        this.#app.post("/story/add", this.#multer().single('file'), async (req, res) => {
-            const subject = req.body.subject;
-            const story = req.body.story;
-            const year = req.body.year;
-
-            console.log(req.file)
-            if (!req.file) {
-                return res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: "No file was uploaded."});
-            }
-            // Get the buffer of the file
-            const sampleFile = req.file.buffer;
-
-            // Generate a unique file name to avoid overwriting existing files
-            const fileName = uuidv4() + ".jpg";
-            let fileUrl = "";
-            console.log("test")
-            // Write the file to disk
-            fs.writeFile(`${wwwrootPath}/uploads/${fileName}`, sampleFile, async (err) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: err});
+                // If no file was uploaded, send a bad request response
+                if (!buffer) {
+                    return res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: "No file was uploaded." });
                 }
 
-                // Construct the URL of the uploaded file
-                 fileUrl = `${req.protocol}://${req.get("host")}/uploads/${fileName}`;
-                console.log(fileUrl);
+                // Generate a unique filename for the uploaded image
+                const fileName = uuidv4() + ".jpg";
+                // Set the file path where the uploaded image will be saved
+                const filePath = `${wwwrootPath}/uploads/${fileName}`;
+                // Write the uploaded file to disk
+                fs.writeFile(filePath, buffer, async (err) => {
+                    if (err) {
+                        console.log(err);
+                        // If an error occurred while writing to disk, send a bad request response
+                        return res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: err });
+                    }
 
-                 console.log(fileUrl)
-                 let statusCode, message;
-                 try {
-                     const data = await this.#databaseHelper.handleQuery({
-                         query: "INSERT INTO story (body, title, date, image) VALUES (?, ?, ?, ?);",
-                         values: [story, subject, year, fileUrl]
-                     });
+                    // Construct the URL of the uploaded file
+                    const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${fileName}`;
+                    console.log(fileUrl);
 
-                     statusCode = this.#errorCodes.HTTP_OK_CODE;
-                     message = "ok";
-                 } catch (e) {
-                     statusCode = this.#errorCodes.BAD_REQUEST_CODE;
-                     message = {reason: e};
-                 }
-                 res.status(statusCode).json(message);
-            });
+                    try {
+                        // Add the story to the database
+                        await this.#databaseHelper.handleQuery({
+                            query: "INSERT INTO story (body, title, date, image) VALUES (?, ?, ?, ?);",
+                            values: [story, subject, year, fileUrl],
+                        });
 
-
-        })
+                        // Send a success response if the story was added successfully
+                        res.status(this.#errorCodes.HTTP_OK_CODE).json("ok");
+                    } catch (e) {
+                        // If an error occurred while adding the story to the database, delete the uploaded image and send a bad request response
+                        fs.unlink(filePath, (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e });
+                    }
+                });
+            } catch (e) {
+                // If an error occurred while processing the request, send a bad request response
+                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({ reason: e });
+            }
+        });
     }
+
 }
 
 
