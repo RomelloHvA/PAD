de4448207e2d5ebb67fbcf162aa9fd8696ce04d9
Romello <romello.ten.broeke@hva.nl>
2023-03-21 14:31:36 +0100

    Content can almost be loaded in.

--- src/assets/js/controllers/timelineController.js
+++ src/assets/js/controllers/timelineController.js
@@ -1,3 +1,7 @@
+/**
+ * Controller for handling the timeline.
+ * @author Romello ten Broeke
+ */
 import {Controller} from "./controller.js";
 import {storyRepository} from "../repositories/storyRepository.js";
 
@@ -8,8 +12,6 @@ export class TimelineController extends Controller {
     #MIN_SCROLL_YEAR = 1970;
     #MAX_SCROLL_YEAR = new Date().getFullYear();
     #DEFAULT_STORY_NUMBER_ON_PAGE = 2;
-    #scrollPosition;
-
     constructor() {
         super();
         this.#currentScrollYear = this.#MAX_SCROLL_YEAR - this.#DEFAULT_STORY_NUMBER_ON_PAGE;
@@ -21,19 +23,64 @@ export class TimelineController extends Controller {
         this.#createTimelineView = await super.loadHtmlIntoContent(
             "html_views/timeline.html");
         // await this.#storyRepository.getHighestStoryPerYear(this.#currentScrollYear)
-        this.#LoadStory()
         this.#LoadStory();
 
     }
 
+    /**
+     * Loads new stories when the user reaches the end of the page by scrolling
+     * @author Romello ten Broeke
+     */
+
     #LoadStory(){
+        let nextStoryPosition = "left";
+
+        // returns a collection. To specify which one I need use the [0].
+        const storyTemplate = document.getElementsByTagName("template")[0];
+        let templateClone = storyTemplate.cloneNode(true);
+        storyTemplate.content.querySelector(".card-position");
         window.addEventListener("scroll", function () {
-            console.log(window.scrollY);
-            if (window.scrollY >= window.innerHeight){
+
+            console.log(document.body.scrollHeight + "scrollheight");
+
+            /**
+             * Math.ceil is used because the scrollposition when adding more stories gets a decimal number.
+             * Because of this the scroll position will never get higher than the scrollheight. So Math.ceil is used to
+             * round up and reach the maximum height so new content can be loaded.
+             * @type {number}
+             */
+            const scrollPosition = Math.ceil(window.scrollY + window.innerHeight);
+            console.log(window.scrollY + window.innerHeight);
+            if ( scrollPosition >= document.body.scrollHeight){
+
+                nextStoryPosition = setStoryPosition(nextStoryPosition,storyTemplate);
+                document.querySelector(".main-timeline").appendChild(templateClone);
                 console.log("Einde bereikt");
             }
-           return window.scrollY;
+
+
+            function setStoryPosition(nextStoryPosition, template) {
+
+                if (nextStoryPosition === "left"){
+                    template.content.querySelector(".card-position").classList.add("timeline");
+                    template.content.querySelector(".card-position").classList.add(nextStoryPosition);
+
+                    nextStoryPosition = "right";
+                } else {
+                    template.content.querySelector(".card-position").classList.add("timeline");
+                    template.content.querySelector(".card-position").classList.add(nextStoryPosition);
+
+                    nextStoryPosition = "left";
+                }
+                return nextStoryPosition;
+
+            }
+
         });
-    }
+
+        }
+
+    
+
 
 }
