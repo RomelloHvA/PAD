28d5fc3a2f267f533eb05d64545f463218584fb9
Romello <romello.ten.broeke@hva.nl>
2023-05-14 16:43:52 +0200

    start of userstory

---
+++ src/assets/js/controllers/myProfileController.js
@@ -0,0 +1,34 @@
+/**
+ * Controller for handling a logged in user their profile page
+ * @author Romello ten Broeke
+ */
+import {Controller} from "./controller.js";
+import {storyRepository} from "../repositories/storyRepository.js";
+import {UsersRepository} from "../repositories/usersRepository.js";
+
+export class myProfileController extends Controller {
+
+    #userId;
+    #myProfileView;
+    #storyData;
+    #userData;
+    #storyRepository;
+    #usersRepository;
+
+    constructor(userId) {
+        super();
+        this.#storyRepository = new storyRepository();
+        this.#usersRepository = new UsersRepository();
+        this.#userId = userId;
+        this.#setupView().then();
+    }
+
+    async #setupView() {
+        this.#myProfileView = await this.loadHtmlIntoContent("html_views/myProfile.html");
+    }
+
+    #getUserData(userId){
+
+    }
+
+}
