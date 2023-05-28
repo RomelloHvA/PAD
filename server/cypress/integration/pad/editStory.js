85392c181fe9ae23de1b93d66716937459b3d46b
Rosalinde Vester <rosalinde.vester@hva.nl>
2023-05-28 23:13:55 +0200

    bezig met test

--- server/cypress/integration/pad/editStory.js
+++ server/cypress/integration/pad/editStory.js
@@ -12,4 +12,20 @@ describe('editStory tests', () => {
         cy.get(".fa-pencil-alt").first().should("be.visible").click({force: true});
         cy.get("#date").should("exist");
     })
+
+    it("check op alle benodigde velden", () => {
+        cy.get(".fa-pencil-alt").first().should("be.visible").click({force: true});
+        cy.get("#subject").should("exist");
+        cy.get("#date").should("exist");
+        cy.get("#story").should("exist");
+        cy.get("#fileInput").should("exist");
+        cy.get("#myButton").should("exist");
+    })
+
+    it("vul verkeerde dingen in", () => {
+        cy.get(".fa-pencil-alt").first().should("be.visible").click({force: true});
+        cy.get("#subject").type(" ");
+        cy.get("#story").type(" ");
+        cy.get("myButton").click({force: true});
+    })
 })
